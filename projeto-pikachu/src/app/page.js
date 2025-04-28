cf1248a8b3a53822f2549a617ba8307d66a20f8e
RaphaelIniesta <raphael_iniesta@yahoo.com>
2025-04-28 19:03:58 -0300

    first commit

---
+++ projeto-pikachu/src/app/page.js
@@ -0,0 +1,18 @@
+export default function Home() {
+  return (
+    <div>
+      <header>
+        <div className="logo">Logo</div>
+        <button className="nav-button">
+          <h1>Cartas por coleção</h1>
+        </button>
+        <div className="search-icon">🔍</div>
+      </header>
+      <main>
+        <div className="main-card">
+          <p>Carta</p>
+        </div>
+      </main>
+    </div>
+  );
+}
---
+++ projeto-pikachu/src/colecao/page.js
@@ -0,0 +1,37 @@
+export default function Colecao() {
+    return (
+      <div>
+        <header>
+          <div className="logo">Logo</div>
+          <h1>Cartas por coleção</h1>
+          <div className="search-icon">🔍</div>
+        </header>
+  
+        <main>
+          <section className="colecoes">
+            <button>Coleção 1</button>
+            <button>Coleção 2</button>
+            <button>Coleção 3</button>
+            <button>Coleção 4</button>
+            <button>Coleção 5</button>
+          </section>
+  
+          <section className="cartas">
+            <div className="card">Carta</div>
+            <div className="card">Carta</div>
+            <div className="card">Carta</div>
+            <div className="card">Carta</div>
+            <div className="card">Carta</div>
+            <div className="card">Carta</div>
+            <div className="card">Carta</div>
+            <div className="card">Carta</div>
+            <div className="card">Carta</div>
+            <div className="card">Carta</div>
+            <div className="card">Carta</div>
+            <div className="card">Carta</div>
+          </section>
+        </main>
+      </div>
+    );
+  }
+  
